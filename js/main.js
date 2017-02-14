2b759e2090ab92cf62cf0294679f4feda95e3cf2
cr0wbait <etcothran@gmail.com>
2017-02-14 14:34:12 -0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,46 +1,66 @@
 // GO!
 
+//use getComputedStyle() to read the styles
+// use document.querySelector() to set values
+// kudos to Victor for help with syntax & logic
 
 // TASK 1 -- Show/Hide Nav
 document.querySelector('#hide-nav button').addEventListener('click', function(){
 
   var navMen = document.querySelector('.nav-menu')
+  var navStyle = window.getComputedStyle(navMen)
 
-  if (navMen.style.visibility = 'visible' ){
+  if (navStyle.visibility === 'visible' ){
     navMen.style.visibility = 'hidden'
   } else {
     navMen.style.visibility = 'visible'
   }
 })
 
-// this refuses to toggle, though I'm not sure why.
+
 
 // TASK 2 -- Select an Icon
-document.querySelector('#select-items .option').addEventListener('click', function(){
-  var option = document.querySelectorAll('.option')
-  console.log(option.classList)
-  if (option.classList.contains('selected') = false){
-    option.classList.add('selected')
-  } else if (option.classList.contains('selected') = true){
-    option.classList.remove('selected')
+document.querySelector('#select-items').addEventListener('click', function(evt){
+  var targ = evt.target
+  if (targ.className !== 'option'){
+    targ = targ.parentElement
+  }
+  if (targ.classList.contains('selected')){
+      targ.classList.remove('selected')
+  } else {
+    targ.classList.add('selected')
+  }
+})
+
+/// querySelectorAll is ALWAYS!! an ARRAY
+// TASK 3 -- Move Item From List to List
+document.querySelector('#add-vals .add-points').addEventListener('click', function(evt){
+
+  var bigNum = document.querySelector('.total-points')
+  console.log(bigNum)
+  var happening = evt.target
+  console.log(happening)
+  if (happening.className !== 'point' ){
+    console.log('do not touch me there!')
+  } else {
+      var newTotal = Number(bigNum.textContent) + Number(happening.textContent)
+      console.log(newTotal)
+      bigNum.textContent = newTotal
   }
 
 })
 
-// right, well, this is also being stubborn.
-
-// TASK 3 -- Move Item From List to List
-document.querySelectorAll('#add-vals button').addEventListener('click', function(){
-  var bigNum = document.querySelector('total-points')
-  var factorNum = document.querySelector('point')
-  var newTotal = Number(bigNum.textContent) * Number(factorNum.textContent)
-  bigNum.textContent = newTotal
-
-})
-
 // TASK 4 -- Add Guest to List
-document.querySelectorAll('#list-2-list li').addEventListener('click', function(){
-
+ document.querySelector('#list-2-list').addEventListener('click', function(evt){
+   var goodList = document.querySelector('.good-standing-list')
+   var badList = document.querySelector('.probation-list')
+   var targ = evt.target
+   console.log(evt)
+   if (targ.parentElement.className === 'good-standing-list'){
+     badList.append(targ)
+   } else if (targ.parentElement.className === 'probation-list'){
+     goodList.append(targ)
+   }
 })
 
 
